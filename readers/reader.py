2f8ca385312f2f4eaac8f0f4c39c308cfef71ec6
orr21 <oscar.rico101@alu.ulpgc.es>
2025-05-10 17:09:23 +0100

    Remove unncesessary docstrings

--- readers/reader.py
+++ readers/reader.py
@@ -2,7 +2,6 @@ from abc import ABC, abstractmethod
 import pandas as pd
 
 class Reader(ABC):
-    # Patron Builder
     @abstractmethod
     def read(self, path: str) -> pd.DataFrame:
         pass
--- writers/writer.py
+++ writers/writer.py
@@ -2,7 +2,6 @@ from abc import ABC, abstractmethod
 import pandas as pd
 
 class Writer(ABC):
-    # Patron Builder
     @abstractmethod
     def write(self, filename: str, data: pd.DataFrame) -> None:
         pass
