f706580b3c5cad3b017c4542d4c8cd53f7268999
orr21 <oscar.rico101@alu.ulpgc.es>
2025-05-10 14:27:26 +0100

    Init commit

--- writers/writer.py
+++ writers/writer.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 import pandas as pd
 
 class Writer(ABC):
+    # Patron Builder
     @abstractmethod
     def write(self, filename: str, data: pd.DataFrame) -> None:
         pass
