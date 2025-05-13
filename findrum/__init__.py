ac7eea852c58df6968da7e2a198fc8fc9890591a
Óscar Rico Rodríguez <95436834+orr21@users.noreply.github.com>
2025-05-13 18:01:55 +0100

    Fix __init__.py

--- findrum/__init__.py
+++ findrum/__init__.py
@@ -1,9 +1,9 @@
-from .crawlers.yahoo_crawler import YahooCrawler
-from .crawlers.sec_crawler import SecCrawler
-from .clients.local_client import LocalDataClient as LocalClient
-from .clients.minio_client import MinioDataClient as MinioClient
-from .readers.parquet_reader import ParquetReader
-from .writers.parquet_writer import ParquetWriter
+from crawlers.yahoo_crawler import YahooCrawler
+from crawlers.sec_crawler import SecCrawler
+from clients.local_client import LocalDataClient as LocalClient
+from clients.minio_client import MinioDataClient as MinioClient
+from readers.parquet_reader import ParquetReader
+from writers.parquet_writer import ParquetWriter
 
 __all__ = [
     "YahooCrawler",
